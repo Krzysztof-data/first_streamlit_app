b5bd688c9e669f84a46e71ba7be1e9ba167e6dcf
KS_data <krzysztofsadowskifrontend@gmail.com>
2023-09-07 11:49:57 +0200

    Update streamlit_app.py
    set fruit name column as the index

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,11 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-#Let's put a pick list here so they can pick the fruit they want ti include
+# choose the fruit name column as the index
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Let's put a pick list here so they can pick the fruit they want ti include
 st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-#display the table on the page
+# display the table on the page
 st.dataframe(my_fruit_list)
