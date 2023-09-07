164a29dc7aace3402f2d94a1e63925e2530c2892
KS_data <krzysztofsadowskifrontend@gmail.com>
2023-09-07 12:27:00 +0200

    Update streamlit_app.py
    fixed typo Strawberries

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 #st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberry'])
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display the table on the page
