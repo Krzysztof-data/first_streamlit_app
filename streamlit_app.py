4e71a8f2919e3f890329a053345d725b586724fb
KS_data <krzysztofsadowskifrontend@gmail.com>
2023-09-12 13:05:21 +0200

    Update streamlit_app.py
    added normalized json format

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,4 +31,12 @@ st.dataframe(fruits_to_show)
 st.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+st.text(fruityvice_response.json()) # just writes the data to the screen
+
+# take the json version of the response and normalize it
+
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+
+#output it the screen as a table
+
+st.dataframe(fruityvice_normalized)
